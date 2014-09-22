56012ca287c63f5e0b05dac1af2b7bceb2d1a948
Pierig Le Saux <lesaux@pythian.com>
2014-09-22 11:07:53 -0400

    indentation

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,28 +1,28 @@
 class flapjack::service {
 
-  service { 'flapjack':
-    ensure     => running,
-    enable     => true,
-    hasstatus  => true,
-    hasrestart => false,
-    require    => [
-      File['/etc/flapjack/flapjack_config.yaml'],
-    ],
-    subscribe  => [
-      Package['flapjack'],
-      File['/etc/flapjack/flapjack_config.yaml'],
-    ]
+service { 'flapjack':
+  ensure     => running,
+  enable     => true,
+  hasstatus  => true,
+  hasrestart => false,
+  require    => [
+    File['/etc/flapjack/flapjack_config.yaml'],
+  ],
+  subscribe  => [
+    Package['flapjack'],
+    File['/etc/flapjack/flapjack_config.yaml'],
+  ]
   }
 
-if $flapjack::embedded_redis { 
-  service { 'redis-flapjack':
-    ensure     => running,
-    enable     => true,
-  }
-} else {
+if $flapjack::embedded_redis {
   service { 'redis-flapjack':
     ensure     => stopped,
     enable     => false,
+  }
+} else {
+  service { 'redis-flapjack':
+    ensure     => running,
+    enable     => true,
   }
 }
 
